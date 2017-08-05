6273d5bf2c156660419be88aacd2dafe86eb1c08
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-08-06 11:45:19 +1200

    playable version of game in dev

--- pkg/R/ham_year.R
+++ pkg/R/ham_year.R
@@ -1,31 +1,31 @@
 
 
 #' @param state list representing the current state of the city, with elements 
-#' acres_owned, acres_planted, people, bushells_fed, bushells_store
+#' acres_owned, acres_planted, people, bushels_fed, bushels_store
 #' @value a new value of state
 #' @details corresponds to the code starting at line 511 of the BASIC version
 ham_year <- function(state){
-  # state <- list(acres_owned = 1000, acres_planted =500, people =100, bushells_fed =1800, bushells_store = 200)
+  # state <- list(acres_owned = 1000, acres_planted =500, people =100, bushels_fed =1800, bushels_store = 200)
   # Harvest
   yield <- sample(1:5, 1)
   harvest <- yield * state$acres_planted
   
   # Rats
   if(runif(1) > 0.6){
-    rats_eaten <- state$bushells_store / sample(c(2, 4), 1)
+    rats_eaten <- floor(state$bushels_store / sample(c(2, 4), 1))
   } else {
     rats_eaten <- 0
   }
   
-  state$bushells_store <- state$bushells_store + harvest - rats_eaten
+  state$bushels_store <- state$bushels_store + harvest - rats_eaten
   state$acres_planted <- 0
   
   # Population changes
-  starved <- floor(state$people - state$bushells_fed / 20)
-  state$bushells_fed <- 0
+  starved <- max(floor(state$people - state$bushels_fed / 20), 0)
+  state$bushels_fed <- 0
   
   new_people <- floor(sample(1:5, 1) * 
-                        (20 * state$acres_owned + state$bushells_store) / 
+                        (20 * state$acres_owned + state$bushels_store) / 
                         state$people / 100 + 1)
 
   state$people <- state$people + new_people - starved
